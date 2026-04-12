82d012eda3b81a264e717f265e827632a7a42c46
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-04-12 15:47:04 +0530

    Wave 2

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -5,7 +5,6 @@ import { WagmiProvider } from "wagmi";
 import { arbitrumSepolia, baseSepolia, sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@rainbow-me/rainbowkit/styles.css";
-import dynamic from "next/dynamic";
 
 const config = getDefaultConfig({
   appName: "Confidro",
